00838f4303ca88a0d598ea1f3684a92bccc93676
Bhopal Singh <bhopalsinghsis@gmail.com>
2021-11-18 09:13:10 -0500

    Update aws-serverless.test.js

--- test/aws-serverless.test.js
+++ test/aws-serverless.test.js
@@ -2,7 +2,7 @@ const { expect, matchTemplate, MatchStyle } = require('@aws-cdk/assert');
 const cdk = require('@aws-cdk/core');
 const AwsServerless = require('../lib/aws-serverless-stack');
 
-test('Empty Stack', () => {
+test('Empty Stack - State beofore runing ckd init', () => {
     const app = new cdk.App();
     // WHEN
     const stack = new AwsServerless.AwsServerlessStack(app, 'MyTestStack');
